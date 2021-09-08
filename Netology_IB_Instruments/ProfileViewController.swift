383d60b773f304ed202dcbdcc4378f8306cde813
AlenAvako <alen.madwatch@gmail.com>
2021-09-08 16:23:42 +0300

    Дополнил д/з

--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -12,7 +12,8 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let profileView = (Bundle.main.loadNibNamed("ProfileView", owner: self, options: nil)![0])
+        self.view.addSubview(profileView as! ProfileView)
     }
     
 
